7a2bf08a27e1030d012041793390c006be237e4b
Noh Tewolde <nohtewolde@nohs-mbp.attlocal.net>
2019-03-19 18:21:43 -0500

    Working Contact list V1.0

---
+++ ContactApplication/ContactDetailCell.swift
@@ -0,0 +1,26 @@
+//
+//  ContactDetailCell.swift
+//  ContactApplication
+//
+//  Created by Noh Tewolde on 3/19/19.
+//  Copyright © 2019 Noh Tewolde. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailCell: UITableViewCell {
+
+    @IBOutlet weak var PhoneNumber: UILabel!
+    @IBOutlet weak var ContactName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ContactApplication/ViewController.swift
+++ ContactApplication/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, DetailVCDelegate {
+    
+    @IBOutlet weak var tbl: UITableView!
+    var contactList = [Person(name : "Anna", phone : "123456"), Person(name : "Bella", phone : "234567"),Person(name : "Cendrella", phone : "345678"), Person(name : "Diana", phone : "456789"), Person(name : "Elsa", phone : "567890")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func passingStringValue(name: String, number: String) {
+        contactList.append(Person(name: name, phone: number))
+        print("In VC \(name)")
+        tbl.reloadData()
+    }
+    
+    @IBAction func btnAddContact(_ sender: UIBarButtonItem) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "DetailVCViewController") as? DetailVCViewController
+        vc?.delegate = self
+        navigationController?.pushViewController(vc!, animated: true)
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let c = tbl.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? ContactDetailCell
+        
+        c?.ContactName.text = contactList[indexPath.row].name
+        c?.PhoneNumber.text = contactList[indexPath.row].phone
+        return c!
     }
 
 
+
+}
+
+
+
+struct Person {
+    var name : String = String()
+    var phone : String = String()
+    init(name : String, phone : String){
+        self.name = name
+        self.phone = phone
+    }
 }
 
